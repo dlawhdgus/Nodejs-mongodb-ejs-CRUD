c60bfe903e56b39efcf884cd1866d985835fc1f4
dlawhdgus <212whdgus@gmail.com>
2022-09-27 11:41:40 +0900

    t

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
-const port = 3000
+const port = process.env.PORT || 3000
 const mongodb = require('mongodb')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
