cd30ac9954c128bc79d0f6b168fbd5b4f85b941b
dlawhdgus <212whdgus@gmail.com>
2022-10-03 10:48:00 +0900

    init commit

--- app.js
+++ app.js
@@ -18,100 +18,151 @@ MongoClient.connect(config.MONGODB_CONNECT_STRING, function(err, db) {
     if (err) throw err;
     const dbo = db.db("db");
     const db_article = dbo.collection("article")
+    const db_todo = dbo.collection("todo")
 
     app.get('/',(req,res) => {
-        res.render('index')
+        try {
+            res.render('index')
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
     })
 
     app.get('/create_article',(req,res) => {
-        res.render('create_article')
+        try {
+            res.render('create_article')
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
     })
 
     app.post('/board',(req,res) => {
-        const {title,nickname,body} = req.body
-        const filter = {}
-        if(!title) {return res.status(400).send('title 입력 필수!')}
-        else{
-            filter.title = title
-            filter.createAt = new Date().toUTCString()
-        }
-        if(!nickname) {return res.status(400).send('nickname 입력 필수!')}
-        else{filter.nickname = nickname}
-        if(!body) {return res.status(400).send('body 입력 필수!')}
-        else{filter.body = body}
+        try {
+            const {title,nickname,body} = req.body
+            const filter = {}
+            if(!title) {return res.status(400).send('title 입력 필수!')}
+            else{
+                filter.title = title
+                filter.createAt = new Date().toUTCString()
+            }
+            if(!nickname) {return res.status(400).send('nickname 입력 필수!')}
+            else{filter.nickname = nickname}
+            if(!body) {return res.status(400).send('body 입력 필수!')}
+            else{filter.body = body}
 
-        db_article.insertOne(filter,(err,result) => {
-            if(err) throw err
-            res.redirect('/read_article')
-        })
+            db_article.insertOne(filter,(err,result) => {
+                res.redirect('/read_article')
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
     })
 
     app.get('/read_article',(req,res) => {
-        db_article.find({},{projection : {}}).toArray((err,result) => {
-            if(err) throw err
-            res.render('read_article',{result})
-        })
-    })
-
-
-
-    app.get('/read_article/:_id',(req,res) => {
-        const {_id} = req.params
-        db_article.find(mongodb.ObjectId(_id),{projection : {_id:0,title:1,nickname:1,body:1,createAt:1}}).toArray((err,result)=> {
-            if(err) throw err
-            res.render('read_article_id',{result})
-        })
-    })
-
-    app.get('/update_article/:_id',(req,res) => {
-        const {_id} = req.params
-        db_article.find({_id : mongodb.ObjectId(_id)},{projection : {}}).toArray((err,result) => {
-            if(err) throw err
-            res.render('update_article',{result, _id : _id})
-        })
-    })
-
-    app.post('/update_board/:_id',(req,res) => {
-        const {_id} = req.params
-        const {title,nickname,body} = req.body
-        const filter = {}
-        if(!title) {return res.status(400).send('title 입력 필수!')}
-        else{
-            filter.title = title
-            filter.createAt = new Date().toUTCString()
+        try {
+            db_article.find({},{projection : {}}).toArray((err,result) => {
+                res.render('read_article',{result})
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
         }
-        if(!nickname) {return res.status(400).send('nickname 입력 필수!')}
-        else{filter.nickname = nickname}
-        if(!body) {return res.status(400).send('body 입력 필수!')}
-        else{filter.body = body}
-        db_article.updateOne({_id : mongodb.ObjectId(_id)},{$set : filter},(err,result) => {
-            if(err) throw err
-            res.redirect('/read_article')
-        })
-        
     })
 
-    app.post('/delete_article/:_id',(req,res) => {
-        const {_id} = req.params
-        db_article.deleteOne({_id : mongodb.ObjectId(_id)},(err,result) => {
-            if(err) throw err
-            res.redirect('/read_article')
-        })
+
+
+    app.get('/read_article/:id',(req,res) => {
+        try {
+            const {id} = req.params
+            db_article.find(mongodb.ObjectId(id),{projection : {_id:0,title:1,nickname:1,body:1,createAt:1}}).toArray((err,result)=> {
+                res.render('read_article_id',{result})
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
     })
 
-    app.post('/delete_admin_article',(req,res) => {
-        const {pw} = req.body
-        console.log(pw)
-        db_article.find({},{projection : {}}).toArray((err,result) => {
-            if(err) throw err
-            res.render('read_admin_article',{result})
-        })
-        
+    app.get('/update_article/:id',(req,res) => {
+        try {
+            const {id} = req.params
+            db_article.find({_id : mongodb.ObjectId(id)},{projection : {}}).toArray((err,result) => {
+                res.render('update_article',{result, _id : id})
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
     })
+
+    app.post('/update_board/:id',(req,res) => {
+        try {
+            const {id} = req.params
+            if(id.length !== 24) res.status(400).send('Bad Request')
+            const {title,nickname,body} = req.body
+            const updateQuery = {}
+            if((title && typeof title === 'string') || (nickname && typeof nickname === 'string') || (body && typeof body === 'string')){
+                if(title) updateQuery.title = title
+                if(nickname) updateQuery.nickname = nickname
+                if(body) {
+                    updateQuery.body = body
+                    updateQuery.createAt = new Date().toUTCString()
+                }
+            }
+            else res.status(400).send('Bad Request')
+            db_article.updateOne({_id : mongodb.ObjectId(id)},{$set : updateQuery},(err,result) => {
+                if(result.matchedCount < 1) res.status(404).send('Not Found')
+                else res.redirect('/read_article')
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
+    })
+
+    app.post('/delete_article/:id',(req,res) => {
+        try {
+            const {id} = req.params
+            if(id.length !== 24) res.status(400).send('Bad Request')
+            db_article.deleteOne({_id : mongodb.ObjectId(id)},(err,result) => {
+                if(result.deletedCount < 1) res.status(404).send('Not Found')
+                else res.redirect('/read_article')
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
+    })
+
+///////////////////////////TODO-LIST//////////////////////////////////////////////////////////////////////
+
+    app.get('/todo_list', (req, res) => {
+        try {
+            db_todo.find({},{projection : {}}).sort({createAt : -1}).toArray((err,result) => {
+                res.render('todo_list',{result})
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
+    })
+
+    app.post('/create_list', (req, res) => {
+        try {
+            const { list } = req.body
+            const filter = {}
+            if(!list) {return res.status(400).send('title 입력 필수!')}
+            else{
+                filter.list = list
+                filter.createAt = new Date().toUTCString()
+            }
+            db_todo.insertOne(filter,(err,result) => {
+                res.redirect('todo_list')
+            })
+        } catch (error) {
+            res.status(500).send('Internal Server Error')
+        }
+    })
+
+    app.all('*', (req, res) => {
+        res.status(404).send('Not Found')
+    })
+
     app.listen(port, ()=> {
       console.log('server on!!')
     })
-
-    app.use((req,res,next) => {res.sendStatus(404)})
-    app.use((err,req,res,next) => {res.sendStatus(500)})
 });
